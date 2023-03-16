3780716bcadfc9a523d7251e28777f5c6e8c9d7c
Kateryna Arkhypiuk <k.arkhypiuk.ia23@kpi.ua>
2023-03-17 00:33:35 +0200

    Lab 3

--- Lab 3/graphics.py
+++ Lab 3/graphics.py
@@ -1,21 +1,18 @@
 import numpy as np
-from scipy.interpolate import CubicSpline, interp1d
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
 x = np.array([0.1, 0.5, 0.9, 1.3, 1.7])
 f = np.array([100.0, 4.0, 1.2346, 0.59172, 0.34602])
 
 cs = CubicSpline(x, f, bc_type='natural')
-ls = interp1d(x, f, kind='linear')
 
 x_star = 8
 print(f'f({x_star}) = {cs(x_star)}')
 
 xx = np.linspace(0.1, 1.7, 1000)
 yy_cub = cs(xx)
-yy_lin = ls(xx)
 plt.plot(xx, yy_cub, label='Cubic Spline')
-plt.plot(xx, yy_lin, label='Linear Spline')
 plt.plot(x, f, 'o', label='Data points')
 plt.legend()
 plt.show()
